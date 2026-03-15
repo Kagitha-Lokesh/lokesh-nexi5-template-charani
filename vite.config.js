f1700bc5e6bd84b5895a9c010ea3c09d7d881bde
Lokesh Kagitha <lokeshk.charani@gmail.com>
2026-03-15 22:44:58 +0530

    Optimize build configuration: increase chunk size limit and implement manual vendor chunking

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,15 @@ export default defineConfig({
   },
   build: {
     sourcemap: false,
-    chunkSizeWarningLimit: 1000,
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
   },
 })
